3a33da2844f39a0162e9fb9339d6693e3c0513b0
Jimmy Clairy <jimmyclairy@gmail.com>
2024-01-29 16:39:48 +0100

    Add js doc and try catch

--- js/functionsLocalStorage.js
+++ js/functionsLocalStorage.js
@@ -1,23 +1,38 @@
+/**
+ * Retrieves the list of cities stored in the local storage.
+ * @returns {Array} - An array of city objects.
+ * @throws {Error} - Throws an error if there is an issue parsing the stored cities.
+ */
 export function getCities() {
-    const getCities = localStorage.getItem('Cities')
+    const storedCities = localStorage.getItem('Cities');
 
-    if (getCities === null) {
-        return []
+    try {
+        return storedCities ? JSON.parse(storedCities) : [];
+    } catch (error) {
+        console.error('Error parsing stored cities:', error);
+        throw new Error('Error retrieving cities from local storage');
     }
-
-    return JSON.parse(getCities)
 }
 
+/**
+ * Adds a city to the list of stored cities in the local storage.
+ * @param {Object} weatherData - The weather data object for the city.
+ * @param {string} weatherData.name - The name of the city.
+ * @param {string} weatherData.temp - The temperature of the city.
+ * @param {Object} weatherData.weather - The weather information for the city.
+ */
 export function addCity(weatherData) {
-    const { name, temp, weather } = weatherData
+    const { name, temp, weather } = weatherData;
 
-    let cities = getCities()
-    cities = cities.filter(objet => objet.name !== name);
-    cities.unshift({ name, temp, weather })
+    let cities = getCities();
+    cities = cities.filter(({ name: cityName }) => cityName !== name);
+    cities.unshift({ name, temp, weather });
 
-    if (cities.length > 5) {
-        cities.pop()
+    const MAX_CITIES = 5;
+
+    if (cities.length > MAX_CITIES) {
+        cities.pop();
     }
 
-    localStorage.setItem('Cities', JSON.stringify(cities))
+    localStorage.setItem('Cities', JSON.stringify(cities));
 }
